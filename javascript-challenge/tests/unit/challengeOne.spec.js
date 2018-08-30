bc921f36036b1dd3f8c0c2fe04d12cd7ff086261
Big Dan <fusebox13@gmail.com>
2018-08-29 23:08:40 -0400

    Fixed the unit tests

--- javascript-challenge/tests/unit/challengeOne.spec.js
+++ javascript-challenge/tests/unit/challengeOne.spec.js
@@ -2,9 +2,6 @@ import { expect } from "chai";
 import { participants } from "@/code-challenge/registration";
 import { CHALLENGE_ONE_TEST_CASES } from './testcases';
 
-const input = [1, 1, 2, 3, 3, 3, 4, 5, 6, 7];
-const expectedOutput = [1, 2, 3, 4, 5, 6, 7];
-
 describe.only("Challenge One", () => {
   participants.forEach(participant => {
     context(participant.name + "'s solution", () => {
